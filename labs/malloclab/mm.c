fce3cdd10cdb259a912d9b2ca322f2b7f9a212e0
Joseph Lee <lijiateng1219@gmail.com>
2025-03-16 09:00:31 +0800

    lab: implicit free list

--- labs/malloclab/mm.c
+++ labs/malloclab/mm.c
@@ -32,8 +32,7 @@ team_t team = {
     /* Second member's full name (leave blank if none) */
     "",
     /* Second member's email address (leave blank if none) */
-    ""
-};
+    ""};
 
 #define WSIZE 4
 #define DSIZE 8
@@ -45,8 +44,33 @@ team_t team = {
 
 // 将内存块大小和分配信息打包到一起，放到 header/footer 里
 // 这里 size 是向上取整过的，末尾 3 位都是 0，size | isAllocated 是可以正常工作的
-#define PACK(size, isAllocted) ((size) | (isAllocted)) 
+#define PACK(size, isAllocted) ((size) | (isAllocted))
 
+// 对给定地址的取值和赋值操作
+// 传入的一般可能是 void* 的泛型指针，在使用的时候需要转换成具体的类型才能解引用
+#define GET(p) (*(unsigned int *)(p))
+#define PUT(p, val) (*(unsigned int *)(p) = (val))
+
+// 从给定的地址（header 或者 footer）中取出存储的 block size 以及分配信息
+#define GET_SIZE(p) (GET(p) & ~0x7))
+#define GET_ALLOC(p) (GET(p) & 0x1)
+
+// 下面操作的都是 block pointer，即跳过 header 字后返回给用户的地址值
+// 1. 存储的 block size = header(WSIZE) + payload + footer(WSIZE);
+// 2. 操作 bp 的时候需要先转为 char* 才能进行算数运算
+// 用户得到的地址值减去一个字长就是 header 所在的位置
+#define HDRP(bp) ((char *)(bp) - WSIZE)
+// 从 bp 开始，加上 block size，再减去 header+footer 的双字偏移，即为 footer 地址
+#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+
+// 找到相邻块的地址信息
+// 后一个相邻块地址：当前位置 + 当前块的 size 信息即可
+// 正好可以把当前块的 footer 和下一个块的 header 跳过去
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)))
+// 前一个相邻块地址 
+// 1. 当前位置向前移动双字，找到前一个块的 footer 后取出前一个块的大小；
+// 2. 当前位置减去这个大小就正好可以得到前一个内存块的位置；
+#define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE((char *)(bp) - DSIZE))
 
 /*
  * mm_init - initialize the malloc package.
